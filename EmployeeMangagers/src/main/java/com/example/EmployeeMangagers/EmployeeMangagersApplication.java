21f55afe4066612d864fc2cdccd1a89491403689
shekhar <jundalesomshekhar@gmail.com>
2021-07-28 13:12:55 +0530

    initial commit

---
+++ EmployeeMangagers/src/main/java/com/example/EmployeeMangagers/EmployeeMangagersApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeMangagers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMangagersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMangagersApplication.class, args);
+	}
+
+}
---
+++ EmployeeMangagers/src/test/java/com/example/EmployeeMangagers/EmployeeMangagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeMangagers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMangagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
